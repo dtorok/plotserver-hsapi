dc2de6c54451976269feec6f8a4444ef27bc2d0b
Daniel Torok <daniel.torok@prezi.com>
2013-09-29 00:44:57 +0200

    Api functions return with PlotData instead of String

--- Plotserver/Api.hs
+++ Plotserver/Api.hs
@@ -1,19 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Plotserver.Api (url, cat, update, delete) where
 
 import Network.Curl
 import Control.Applicative
+
+import Plotserver.Types
 
 ------ API ------
 
 url :: String -> String
 url dataset = "https://plot.prezi.com/" ++ dataset
 
-cat :: (String, String) -> String -> IO (Either String String)
+cat :: (String, String) -> String -> IO (Either PlotData String)
 cat auth dataset = getResult <$> curlGetResponse_ url_ opts where
 	url_ = actionUrl dataset "download"
 	opts = defaultOpts auth
 
-update :: (String, String) -> String -> String -> String -> IO (Either String String)
+update :: (String, String) -> String -> String -> String -> IO (Either PlotData String)
 update auth dataset key values = getResult <$> curlGetResponse_ url_ opts where
 	postData = key ++ "," ++ values
 	url_ = actionUrl dataset "update"
@@ -22,7 +26,7 @@ update auth dataset key values = getResult <$> curlGetResponse_ url_ opts where
 		CurlPostFields [postData]
 		]
 
-delete :: (String, String) -> String -> IO (Either String String)
+delete :: (String, String) -> String -> IO (Either PlotData String)
 delete auth dataset = getResult <$> curlGetResponse_ url_ opts where
 	url_ = actionUrl dataset "delete"
 	opts = defaultOpts auth
@@ -37,10 +41,10 @@ defaultOpts (username, password) = [
 	]
 
 actionUrl :: String -> String -> String
-actionUrl dataset action = (url dataset) ++ "?" ++ action
+actionUrl dataset action = url dataset ++ "?" ++ action
 
-getResult :: CurlResponse_ [(String, String)] body -> Either body String
+getResult :: CurlResponse_ [(String, String)] String -> Either PlotData String
 getResult response 
-	| respStatus response == 200 = Left $ respBody response
+	| respStatus response == 200 = Left $ (read (respBody response) :: PlotData)
 	| otherwise = Right err where
-		err = "Error: " ++ (show $ respStatus response)
+		err = "Error: " ++ show (respStatus response)
